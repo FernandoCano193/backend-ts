de60665a7d8d28ad169543c08be1c3156d0542e1
Fernando Cano <fernandocano981@gmail.com>
2025-04-01 17:52:54 -0700

    chore: commitlint install

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,8 +11,7 @@ export default defineConfig([
   tseslint.configs.recommended,
   {
     rules:{
-      "no-unused-vars": "warn",
-			"no-console": "warn",
+      "no-unused-vars": "warn"
     }
   }
 ]);
